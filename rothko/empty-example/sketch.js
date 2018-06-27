33ebd3f3cd9fda31331a60063d16b55d7c5b69fe
Unknown <stepan.zadnik@mail.muni.cz>
2018-06-27 18:37:29 +0200

    Jsem ještě větší umělkyně

---
+++ rothko/empty-example/sketch.js
@@ -0,0 +1,14 @@
+// do not remove, allowes autocomplete
+/// <reference path="./../p5.global-mode.d.ts" />
+
+function setup() {
+  createCanvas(400,400);
+  background("red");
+  fill("aquamarine");
+  rect(10,10,100,100);
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+}
