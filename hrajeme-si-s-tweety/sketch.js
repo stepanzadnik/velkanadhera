97d7c9dec74703f1d94e473fe40c02aa55f4ae09
Unknown <stepan.zadnik@mail.muni.cz>
2018-06-28 16:19:01 +0200

    Tweety

--- hrajeme-si-s-tweety/sketch.js
+++ hrajeme-si-s-tweety/sketch.js
@@ -2,6 +2,8 @@
 /// <reference path="./../p5.global-mode.d.ts" />
 
 function setup() {
+  var vysledek = tweets[0].text.replace("rep", "hip hop");
+  console.log(vysledek);
   // put setup code here
   createCanvas(400, 400);
   rect(10, 10, 100, 100);
