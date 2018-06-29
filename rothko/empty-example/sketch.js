7427798f15e9a988fee2e10b1f349a55623b5293
Unknown <stepan.zadnik@mail.muni.cz>
2018-06-29 16:01:30 +0200

    Pacman se hýbe

--- rothko/empty-example/sketch.js
+++ rothko/empty-example/sketch.js
@@ -1,11 +1,12 @@
 // do not remove, allowes autocomplete
 /// <reference path="./../p5.global-mode.d.ts" />
 
-var x = 15;
-var y = 1;
-var z = 0;
-var time = 0;
-var timespeed = 1;
+var ham = 15;
+var pant = 0;
+var pohybV = 0;
+var pohybH = 0;
+var poziceX = 314;
+var poziceY = 314;
 
 function setup() {
   createCanvas(627,627);
@@ -13,53 +14,121 @@ function setup() {
 }
 
 function draw() {
-  var poziceY = time % 627;
-  time = time + timespeed;
-  background (255,255,0,20);
-  fill("aquamarine");
-  stroke(0,0,255);
-  //rect(10, 10, 100 + 10 * x, 100 + 10 * y);
+  poziceX = (poziceX + pohybH) % 627;
+  poziceY = (poziceY + pohybV) % 627;
+  if (poziceX < 0) {
+    poziceX = poziceX + 627;
+  }
+  if (poziceY < 0) {
+    poziceY = poziceY + 627;
+  }
+  background (255,255,0,80);
   stroke(0,0,0);
   fill(255,0,0);
-  arc(poziceY, 50, 50, 50, 0, PI + HALF_PI + HALF_PI / (15/x), PIE);
+  pacman(poziceX, poziceY, pohybH, pohybV);
   pusa();
-  if (z == 1) {
-x = x - 1;
+  if (pant == 1) {
+ham = ham - 1;
   }
 else {
-x = x + 1;
+ham = ham + 1;
 
   }
-  if (keyIsPressed === true) {
-    timespeed == 100;
-  }
-  else {
-    timespeed == 1;
-  }
+
   // put drawing code here
 }
 
-function pusa () {
-if (x == 15) {
-  z = 1;
+function pacman(x, y, pohybH, pohybV) {
+  
+  push();
+  translate(x, y);
+  rotate(-PI/6);
+  if (pohybV == 0 && pohybH > 0) {
+    rotate(0);
+  }
+    else if  (pohybV == 2 && pohybH == 4) {
+      rotate(0.125 * PI);
+    }
+    else if  ((pohybV == 2 && pohybH == 2)||(pohybV == 4 && pohybH == 4)) {
+      rotate(0.25 * PI);
+    }
+    else if  (pohybV == 4 && pohybH == 2) {
+      rotate(0.375 * PI);
+    }
+    else if (pohybV > 0 && pohybH == 0) {
+      rotate(0.5 * PI);
+    }
+    else if  (pohybV == 4 && pohybH == -2) {
+      rotate(0.625 * PI);
+    }
+    else if  ((pohybV == 2 && pohybH == -2)||(pohybV == 4 && pohybH == -4)) {
+      rotate(0.75 * PI);
+    }
+    else if  (pohybV == 2 && pohybH == -4) {
+      rotate(0.875 * PI);
+    }
+    else if (pohybV == 0 && pohybH < 0) {
+      rotate(PI);
+    }
+    else if  (pohybV == -2 && pohybH == -4) {
+      rotate(1.125 * PI);
+    }
+    else if  ((pohybV == -2 && pohybH == -2)||(pohybV == -4 && pohybH == -4)) {
+      rotate(1.25 * PI);
+    }
+    else if  (pohybV == -4 && pohybH == -2) {
+      rotate(1.375 * PI);
+    }
+    else if (pohybV < 0 && pohybH == 0) {
+      rotate(1.5 * PI);
+    }
+    else if  (pohybV == -4 && pohybH == 2) {
+      rotate(1.625 * PI);
+    }
+    else if  ((pohybV == -2 && pohybH == 2)||(pohybV == -4 && pohybH == 4)) {
+      rotate(1.75 * PI);
+    }
+    else if  (pohybV == -2 && pohybH == 4) {
+      rotate(1.875 * PI);
+    }
+    
+  
+  arc(0, 0, 50, 50, 15/ham, - (15/ham) + 2.325 * PI, PIE);
+  pop(); 
 }
-else if (x == 5) {
-  z = 0;
+
+function pusa () {
+if (ham == 30) {
+  pant = 1;
+}
+else if (ham == 10) {
+  pant = 0;
 }
 }
 
-function keyReleased () {
-  y = y + 1;
-  if (y >= 50   ) {
-    y = 0;
-      }
-};
 
 function keyPressed() {
   if (keyCode === UP_ARROW) {
-    fillVal = 255;
+    pohybV = pohybV - 2;
+    if (pohybV < -4)  {
+      pohybV = -4;
+    };
   } else if (keyCode === DOWN_ARROW) {
-    fillVal = 0;
-  }
+    pohybV = pohybV + 2;
+    if (pohybV > 4)  {
+      pohybV = 4;
+    };
+  }else if (keyCode === LEFT_ARROW) {
+    pohybH = pohybH - 2;
+    if (pohybH < -4)  {
+      pohybH = -4;
+    };
+  }else if (keyCode === RIGHT_ARROW) {
+    pohybH = pohybH + 2;
+    if (pohybH > 4)  {
+      pohybH = 4;
+    };
+  };
+
   return false; // prevent default
 }
